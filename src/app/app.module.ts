5eb0d16d34992d7a155b2479ef32ba239c065cd5
josealarconchacon <jose.alarcon.chacon@gmail.com>
2023-07-04 14:13:21 -0400

    import FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,18 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './login/login.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [AppComponent, NavBarComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
