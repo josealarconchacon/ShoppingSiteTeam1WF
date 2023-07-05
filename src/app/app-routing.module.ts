68854c763de73bb19e7b97961213f4dce4445db1
josealarconchacon <jose.alarcon.chacon@gmail.com>
2023-07-05 12:39:26 -0400

    add login/register route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
 
 const routers: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'nav-bar', component: NavBarComponent },
 ];
 
