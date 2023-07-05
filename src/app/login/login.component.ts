a156b6431ab249cfaa89090bd1bc2374ffb26839
josealarconchacon <jose.alarcon.chacon@gmail.com>
2023-07-05 12:40:38 -0400

    implement login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  // set variable type FormGroup
+  loginForm: FormGroup;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    // initialize variable to equal new FormGroup
+    this.loginForm = new FormGroup({
+      username: new FormGroup('', [Validators.required]),
+      password: new FormGroup('', [Validators.required]),
+    });
+  }
+
+  onSubmit() {}
+}
