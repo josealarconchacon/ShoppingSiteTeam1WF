dedc3a208ac07f968dfb66948ae3f5dab3c3e5c9
josealarconchacon <jose.alarcon.chacon@gmail.com>
2023-07-05 17:23:46 -0400

    config files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { LoginComponent } from './login/login.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, LoginComponent],
+  declarations: [AppComponent, NavBarComponent, LoginComponent, RegisterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
