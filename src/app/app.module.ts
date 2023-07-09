14fe79d7b9d8660e688b065c3aabecc703f40f0b
Dane Reid <danereid1024@icoud.com>
2023-07-09 19:13:28 -0400

    errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './login/login.component';
 
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+//import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegisterComponent } from './register/register.component';
 import { CollectionsComponent } from './collections/collections.component';
 import { DetailComponent } from './detail/detail.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
-import { FooterComponent } from './footer/footer.component';
-import {  ContactUsComponent } from './contact-us/contact-us.component';
+//import { FooterComponent } from './footer/footer.component';
+//import { ContactUsComponent } from './contact-us/contact-us.component';
 
 
 @NgModule({
@@ -34,14 +34,14 @@ import {  ContactUsComponent } from './contact-us/contact-us.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule,
+    //ReactiveFormsModule,
     FooterComponent,
     ContactUsComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
+  //imports: [
+  //  BrowserModule,
+  //  FormsModule
+ // ],
   providers: [],
   bootstrap: [AppComponent],
 })
