428201f7bce789d0b2f21b0cbb5bb8f7e49a4cc3
josealarconchacon <jose.alarcon.chacon@gmail.com>
2023-07-19 21:52:14 -0400

    working on search method

--- src/app/service/movie.service.ts
+++ src/app/service/movie.service.ts
@@ -7,6 +7,10 @@ import { Observable } from 'rxjs';
 })
 export class MovieService {
   private apiUrl: string = 'https://imdb-top-100-movies.p.rapidapi.com/';
+  private apiKey: string = '809f52d640msh8ba44e260d0fe3ap1b72f0jsnadbfe7ffcafb';
+
+  movies: any[] = [];
+  movieGenre: string = '';
 
   constructor(private http: HttpClient) {}
 
@@ -19,5 +23,13 @@ export class MovieService {
       },
     };
     return this.http.get<any>(this.apiUrl, options);
+  }
+
+  // Still need to fix this block of code for searching movies
+  // search method
+  searchMoviesByTitle(title: string) {
+    const headers = new HttpHeaders().set('X-RapidAPI-Key', this.apiKey);
+    const params = { title: title };
+    return this.http.get(this.apiUrl, { headers: headers, params: params });
   }
 }
