2e3dd2c93cb741dad5f0fff7f0d52195fddf6ac1
josealarconchacon <jose.alarcon.chacon@gmail.com>
2023-07-19 21:52:58 -0400

    still need to fix this component to display the right data

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from '../service/movie.service';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.css'],
 })
-export class DetailComponent {}
+export class DetailComponent implements OnInit {
+  movieId: string;
+  movieData = '';
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(
+      (params) => {
+        this.movieId = params['id'];
+        this.movieData = history.state.movieData ?? 'No Data to display';
+        console.log(`Movie ID: ${this.movieId}`);
+        console.log(`Movie Data: ${this.movieData}`);
+      },
+      (error) => {
+        console.log(`Error getting data: ${error}`);
+      }
+    );
+  }
+}
