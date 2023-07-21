c784f43e82926abfb03a34cfd5ebb5b612b35db7
josealarconchacon <jose.alarcon.chacon@gmail.com>
2023-07-21 12:43:36 -0400

    passing data to detail component

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MovieService } from '../service/movie.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-detail',
@@ -9,20 +10,35 @@ import { MovieService } from '../service/movie.service';
 })
 export class DetailComponent implements OnInit {
   movieId: string;
-  movieData = '';
+  movieData: any;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
   ngOnInit(): void {
     this.route.params.subscribe(
       (params) => {
         this.movieId = params['id'];
-        this.movieData = history.state.movieData ?? 'No Data to display';
+        this.fetchMovieData(this.movieId);
         console.log(`Movie ID: ${this.movieId}`);
         console.log(`Movie Data: ${this.movieData}`);
       },
       (error) => {
         console.log(`Error getting data: ${error}`);
+      }
+    );
+  }
+
+  fetchMovieData(movieID: string) {
+    const apiKey = '809f52d640msh8ba44e260d0fe3ap1b72f0jsnadbfe7ffcafb';
+    const url = `https://imdb-top-100-movies.p.rapidapi.com/${movieID}`;
+
+    this.http.get(url, { headers: { 'X-RapidAPI-Key': apiKey } }).subscribe(
+      (response: any) => {
+        this.movieData = response;
+        console.log(`Movie Data Response: ${this.movieData}`);
+      },
+      (error) => {
+        console.error('Error fetching movie data:', error);
       }
     );
   }
