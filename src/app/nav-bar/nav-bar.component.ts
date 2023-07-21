e724184ae191cbd39d725d9c1f53774a73e3b6b7
josealarconchacon <jose.alarcon.chacon@gmail.com>
2023-07-21 13:50:06 -0400

    navbar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieService } from '../service/movie.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -9,24 +10,22 @@ import { Title } from '@angular/platform-browser';
   providers: [MovieService],
 })
 export class NavBarComponent implements OnInit {
-  searchTitle: string;
-  movies: any[];
-
-  constructor(private movieService: MovieService) {}
+  // searchTitle: string;
+  // movies: any[];
+  constructor(private movieService: MovieService, private http: HttpClient) {}
 
   ngOnInit() {}
-
   // Still need to fix this block of code for searching movies
   // search movies
-  searchMovies() {
-    this.movieService.searchMoviesByTitle(this.searchTitle).subscribe(
-      (response: any) => {
-        (this.movies = response['title']), toString().toLowerCase(); // Assuming the response contains the movie data
-        console.log(this.movies);
-      },
-      (error) => {
-        console.error('An error occurred during the API call:', error);
-      }
-    );
-  }
+  // searchMovies() {
+  //   this.movieService.searchMoviesByTitle(this.searchTitle).subscribe(
+  //     (response: any) => {
+  //       (this.movies = response.title), toString().toLowerCase(); // Assuming the response contains the movie data
+  //       console.log(this.movies);
+  //     },
+  //     (error) => {
+  //       console.error('An error occurred during the API call:', error);
+  //     }
+  //   );
+  // }
 }
